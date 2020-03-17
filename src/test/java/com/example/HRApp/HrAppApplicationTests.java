c837c8fa421cd47236fb980a20bf9e0129474e92
MerveYayın <merve.yayin@kgteknoloji.com>
2020-03-17 16:12:58 +0300

    created project file

---
+++ src/test/java/com/example/HRApp/HrAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HRApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
