1cde851827c7a6460751e96793d42002d90422a4
MerveYayın <merve.yayin@kgteknoloji.com>
2020-03-17 16:35:16 +0300

    added entity package for creating sql tables crating constant package

---
+++ src/main/java/com/example/HRApp/constants/SetUpConstants.java
@@ -0,0 +1,6 @@
+package com.example.HRApp.constants;
+
+public class SetUpConstants {
+    public static final int ID_LENGTH = 50;
+    public static final String MAIL_PATTERN = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+}
