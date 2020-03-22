da624ff93043213b1173895484ab15b0a93e20f9
MerveYayın <merve.yayin@kgteknoloji.com>
2020-03-22 15:31:04 +0300

    hotFix

--- src/web-ui/src/service/job-service.service.ts
+++ src/web-ui/src/service/job-service.service.ts
@@ -2,7 +2,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { JobResource } from '../resource/job-resource';
-import { Observable } from 'rxjs/Observable';
+import { Observable } from 'rxjs';
 import {HttpParams} from "@angular/common/http";
 
 @Injectable()
